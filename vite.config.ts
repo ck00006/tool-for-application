b79a6f6a66a0809e11f6ab2d4c0e21a6e6f9658b
ck00006 <164793597+ck00006@users.noreply.github.com>
2024-04-25 23:48:53 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,33 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import Federation from '@originjs/vite-plugin-federation'
+'./node_modules/lodash'
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(),
+  Federation({
+    name: 'my-app-one',
+    filename: 'remoteEntry.js',
+    exposes: {
+      './lodash': './src/exposes/exposeLodash.js',
+      './moment': './src/exposes/exposeMoment.js',
+    },
+    shared: {
+      react: {
+        singleton: true,
+        requiredVersion: 'deps.react', // Ensure this matches the version in package.json
+      },
+      'react-dom': {
+        singleton: true,
+        requiredVersion: 'deps["react-dom"]', // Ensure this matches the version in package.json
+      },
+      // ... any other shared dependencies
+    }
+  })
+  ],
+  build: {
+    target: 'esnext',
+    sourcemap: true
+  }
+})
 
